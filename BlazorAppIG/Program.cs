7676d089207174c221093eb3cef51d87db2aece4
mkamiishi <mkamiishi@infragistics.com>
2022-12-28 19:04:09 +0900

    初回コミット
    Initial commit

---
+++ BlazorAppIG/Program.cs
@@ -0,0 +1,24 @@
+﻿using BlazorAppIG;
+using IgniteUI.Blazor.Controls;
+using Microsoft.AspNetCore.Components.Web;
+using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+
+var builder = WebAssemblyHostBuilder.CreateDefault(args);
+builder.RootComponents.Add<App>("#app");
+builder.RootComponents.Add<HeadOutlet>("head::after");
+
+builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+
+builder.Services.AddIgniteUIBlazor(
+    typeof(IgbLegendModule),
+    typeof(IgbDataChartCoreModule),
+    typeof(IgbDataChartCategoryModule),
+    typeof(IgbDataChartCategoryCoreModule),
+    typeof(IgbDataChartInteractivityModule),
+    typeof(IgbDataChartStackedModule),
+    typeof(IgbStackedFragmentSeriesModule),
+    typeof(IgbCalloutLayerModule)
+);
+
+
+await builder.Build().RunAsync();
